1682a79a7dd0920f8816f47fb574a30d52410fb2
CreeKsx3 <creeksx3@web.de>
2013-04-19 16:40:05 +0200

    Base Class

---
+++ AT_common/com/creeks/at/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.creeks.at.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "AT";
+    public static final String MOD_NAME = "Advanced Technology";
+    public static final String VERSION = "0.0.1";
+}
